c83bb46e0d62e8297f71f8b546345c5d0085f336
RAMAYANAPU VENKATA PRANAI <pranayramayanapu@gmail.com>
2026-09-03 09:05:22 +0530

    updated dotfiles

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -3,7 +3,7 @@
 -- Add any additional options here
 vim.g.lazyvim_check_order = false
 -- 4-space indentation
--- vim.opt.tabstop = 4 -- A tab displays as 4 spaces
--- vim.opt.shiftwidth = 4 -- Indent size for >> << etc.
--- vim.opt.softtabstop = 4 -- <Tab>/<BS> behave like 4 spaces
--- vim.opt.expandtab = true -- Use spaces instead of actual tabs
+vim.opt.tabstop = 4 -- A tab displays as 4 spaces
+vim.opt.shiftwidth = 4 -- Indent size for >> << etc.
+vim.opt.softtabstop = 4 -- <Tab>/<BS> behave like 4 spaces
+vim.opt.expandtab = true -- Use spaces instead of actual tabs
