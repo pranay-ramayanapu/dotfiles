74f04521c749562994a06196fa3c8f6c325f5182
RAMAYANAPU VENKATA PRANAI <pranayramayanapu@gmail.com>
2026-09-19 12:54:52 +0530

    changed colorscheme to catpuccin

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,20 @@
 return {
-	-- 1. Configure Tokyo Night with transparency
 	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
+		"catppuccin/nvim",
+		lazy = true,
+		name = "catppuccin",
 		opts = {
-			transparent = true,
-			styles = {
-				sidebars = "transparent",
-				floats = "transparent",
+			transparent_background = true,
+			float = {
+				transparent = true,
+				solid = true,
 			},
 		},
 	},
-
-	-- 2. Tell LazyVim to use Tokyo Night as the active colorscheme
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "tokyonight",
+			colorscheme = "catppuccin-mocha",
 		},
 	},
 }
